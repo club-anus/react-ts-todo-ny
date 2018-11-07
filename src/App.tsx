3883a3d4563b490b361fa1c4a07919047fee038d
youtangai <youtanagai@gmail.com>
2018-11-08 00:18:03 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import './App.css';
 import TodoList from './component/TodoList'
 
 class App extends React.Component {
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import {createStore, applyMiddleware} from 'redux';
 import {rootReducer} from './reducer/index';
