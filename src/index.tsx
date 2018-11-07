ba60db3f5839d178f314b10c39162765b80d98a9
youtangai <youtanagai@gmail.com>
2018-11-07 22:46:22 +0900

    add store with provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,18 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import {rootReducer} from './reducer/index';
+import {logger} from 'redux-logger';
+import {Provider} from 'react-redux';
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
