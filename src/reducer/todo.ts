7698712cc17890716defa71cb48fd77ab19c2dee
youtangai <youtanagai@gmail.com>
2018-11-07 23:15:06 +0900

    add add todo logic

--- src/reducer/todo.ts
+++ src/reducer/todo.ts
@@ -10,6 +10,12 @@ export const todoReducer = (state: Todo[], action:TodoAction) => {
     let _state:Todo[] = Object.assign({}, state)
     switch(action.type) {
         case TodoActionType.ADD_TODO:
+            const todo:Todo = {
+                id: action.id,
+                text: action.text,
+                isDone: false,
+            }
+            _state.push(todo)
             return _state
         case TodoActionType.UPDATE_TODO:
             return _state
