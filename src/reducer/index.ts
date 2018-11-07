ba09fd78d23fe92d6fa13f75533da2347acbcc19
youtangai <youtanagai@gmail.com>
2018-11-07 23:41:49 +0900

    add rootstate interface

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,7 +1,11 @@
 import {combineReducers} from 'redux'
 import {todoReducer} from './todo'
+import {Todo} from '../reducer/todo'
 
+export interface IrootState {
+    todoState: Todo[]
+}
 
 export const rootReducer = combineReducers({
-    todo: todoReducer,
+    todoState: todoReducer,
 })
