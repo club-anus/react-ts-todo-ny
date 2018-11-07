a640a449d5611f4afd7a192a24024982f6ca2cb5
youtangai <youtanagai@gmail.com>
2018-11-07 22:40:52 +0900

    add reducer

---
+++ src/reducer/todo.ts
@@ -0,0 +1,21 @@
+import {TodoActionType, TodoAction} from '../action/todo'
+
+export type Todo = {
+    id: number,
+    text: string,
+    isDone: boolean,
+}
+
+export const todoReducer = (state: Todo[], action:TodoAction) => {
+    let _state:Todo[] = Object.assign({}, state)
+    switch(action.type) {
+        case TodoActionType.ADD_TODO:
+            return _state
+        case TodoActionType.UPDATE_TODO:
+            return _state
+        case TodoActionType.DELETE_TODO:
+            return _state
+        default:
+            return _state
+    }
+}
